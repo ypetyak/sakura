05aa4165ba145ab6b63385920e21fcfb16d54537
bsalgautam16 <bishal.gautam@careem.com>
2019-06-16 13:00:43 +0200

    add

--- src/history.js
+++
@@ -1,6 +0,0 @@
-// Here we manually create a hisory object to have
-// easier access to it
-import { createBrowserHistory } from 'history';
-// React Router has already intalled package history
-
-export default createBrowserHistory();
