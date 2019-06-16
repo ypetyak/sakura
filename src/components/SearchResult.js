61cecf36f93e8efca36f502983dae65a0a3a569b
bsalgautam16 <bishal.gautam@careem.com>
2019-06-16 12:38:45 +0200

    style for tweets fixed

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -23,30 +23,37 @@ class SearchResult extends React.Component {
     }
 
     return (
-        <div className="results-div">
-            <div className="score-div">
-                <h2>Your score is:</h2>
-                <div className="number-div">
-                    <p>{this.props.averageScore}</p>
-                </div>
-            </div>
-            <TimeChart tweets={this.props.dateAndScore} />
-            <div className="best-tweet-div">
-                <div className="first-tweet">
-                    <h2>Tweet with highest score {this.props.tweets.goodTweetScore}:
-                    </h2>
-                    <p>{this.props.tweets.goodTweet}</p>
+        <div className="result-main-div">
+            <div className="results-div">
+                <div className="score-div">
+                    <h2>Your score is:</h2>
+                    <div className="number-div">
+                        <p>{this.props.averageScore}</p>
                     </div>
-                    <div className="second-tweet">
-                    <h2>Tweet with lowest score {this.props.tweets.badTweetScore}:
-                    </h2>
-                    <p>{this.props.tweets.badTweet}</p>
                 </div>
+                <TimeChart tweets={this.props.dateAndScore} />
+                <div className="best-tweet-div">
+                    <div className="first-tweet">
+                        <h2>Tweet with highest score {this.props.tweets.goodTweetScore}:
+                        </h2>
+                        <div className="speech-bubble">
+                        <p>{this.props.tweets.goodTweet}</p>
+                        </div>
+                        </div>
+                        <div className="second-tweet">
+                        <h2>Tweet with lowest score {this.props.tweets.badTweetScore}:
+                        </h2>
+                        <div className=" speech-bubble">
+                            <p>{this.props.tweets.badTweet}</p>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
             <div className="subscribe-div">
-                <h2> Subscribe to be notified in your score change: </h2>
-                <input  placeholder="Email" className="subscription-input"/>
-                <button>Subscribe</button>
+                    <h2> Subscribe to be notified in your score change: </h2>
+                    <input  placeholder="Email" className="subscription-input"/>
+                    <button>Subscribe</button>
             </div>
         </div>
     )
